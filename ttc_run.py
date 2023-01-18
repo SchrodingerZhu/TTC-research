f992bf3e66db9a192376eb8892f6cd36b5b107ca
NoyaFangzhou <noyafangzhou.ark@gmail.com>
2023-01-17 19:19:43 -0500

    change the TTC plot to stair pattern add the miss ratio plot in last row

--- ttc_run.py
+++ ttc_run.py
@@ -7,9 +7,9 @@ import argparse
 import subprocess
 import math
 import json
-import pandas as pd
 import os
 import re
+import copy
 from matplotlib.pyplot import *
 
 ROOT = f"."
@@ -79,40 +79,57 @@ def convert_histogram_to_log2(name):
 # {
 # 	row,
 # 	col,
-# 	i: row_id
+# 	i: col_id,
+# 	prev_axes,
 # }
 def plot_ttc_figure_entry(fig, fig_config, json_data, histogram, ttc_label):
 	# plot TTC line
-	ax = fig.add_subplot(fig_config[0],fig_config[1],fig_config[2])
+	ax1 = fig.add_subplot(fig_config[0],fig_config[1],fig_config[2])
 	print(f"{fig_config[0]},{fig_config[1]},{fig_config[2]}")
 	cache_size = list(map(lambda t: t[0]*64/1024, json_data))
-	ttc = list(map(lambda t: t[-1], json_data))
-	ax.plot(cache_size, ttc, label=ttc_label, linestyle="-", linewidth=2, color="#44546A", alpha=1.0)
-	ax.set_xlabel('Cache Size (KB)', fontsize=14, fontweight='bold')
-	ax.set_ylabel('TTC', fontsize=14, fontweight='bold')
+	ttc = list(map(lambda t: t[-2], json_data))
+	ax1.plot(cache_size, ttc, label=ttc_label, linestyle="-", linewidth=2, color="#44546A", alpha=1.0)
+	ax1.set_xlabel('Cache Size (KB)', fontsize=14, fontweight='bold')
+	ax1.set_ylabel('TTC', fontsize=14, fontweight='bold')
 	grid(True)
 	legend()
 	# plot the ce/cs line
 	clb = list(map(lambda t: t[1]*64/1024, json_data))
 	cub = list(map(lambda t: t[2]*64/1024, json_data))
-	ax = fig.add_subplot(fig_config[0],fig_config[1],fig_config[2]+fig_config[1])
+	ax2 = fig.add_subplot(fig_config[0],fig_config[1],fig_config[2]+fig_config[1])
 	print(f"{fig_config[0]},{fig_config[1]},{fig_config[2]+fig_config[1]}")
-	ax.plot(cache_size, clb, label="Cs", linestyle="-", linewidth=2, color="#44546A", alpha=1.0)
-	ax.plot(cache_size, cub, label="Ce", linestyle=":", linewidth=4, color="#C8A593", alpha=1.0)
-	ax.set_xlabel('Cache Size (KB)', fontsize=14, fontweight='bold')
-	ax.set_ylabel('Ce/Cs', fontsize=14, fontweight='bold')
+	ax2.plot(cache_size, clb, label="Cs", linestyle="-", linewidth=2, color="#44546A", alpha=1.0)
+	ax2.plot(cache_size, cub, label="Ce", linestyle=":", linewidth=4, color="#C8A593", alpha=1.0)
+	ax2.set_xlabel('Cache Size (KB)', fontsize=14, fontweight='bold')
+	ax2.set_ylabel('Ce/Cs', fontsize=14, fontweight='bold')
 	grid(True)
 	legend()
 	# plot the histogram
 	ri = sorted(histogram.keys())
 	ri_freq = [histogram[i] for i in ri]
-	ax = fig.add_subplot(fig_config[0],fig_config[1],fig_config[2]+fig_config[1]*2)
+	if fig_config[-1]:
+		ax3 = fig.add_subplot(fig_config[0],fig_config[1],fig_config[2]+fig_config[1]*2, sharey=fig_config[-1][2])
+	else:
+		ax3 = fig.add_subplot(fig_config[0],fig_config[1],fig_config[2]+fig_config[1]*2)
 	print(f"{fig_config[0]},{fig_config[1]},{fig_config[2]+fig_config[1]*2}")
-	ax.scatter(ri, ri_freq, s=16, color="#44546A", alpha=1.0)
-	ax.set_xlabel('RI', fontsize=14, fontweight='bold')
-	ax.set_ylabel('Distribution', fontsize=14, fontweight='bold')
-	ax.set_xscale('log', base=2)
+	ax3.scatter(ri, ri_freq, s=16, color="#44546A", alpha=1.0)
+	ax3.set_xlabel('RI', fontsize=14, fontweight='bold')
+	ax3.set_ylabel('Distribution', fontsize=14, fontweight='bold')
+	ax3.set_xscale('log', base=2)
 	grid(True)
+	# plot the miss ratio
+	miss_ratio = list(map(lambda t: t[-1], json_data))
+	if fig_config[-1]:
+		ax4 = fig.add_subplot(fig_config[0],fig_config[1],fig_config[2]+fig_config[1]*3, sharey=fig_config[-1][3])
+	else:
+		ax4 = fig.add_subplot(fig_config[0],fig_config[1],fig_config[2]+fig_config[1]*3)
+	
+	print(f"{fig_config[0]},{fig_config[1]},{fig_config[2]+fig_config[1]*3}")
+	ax4.plot(cache_size, miss_ratio, linestyle="-", linewidth=4, color="#C8A593", alpha=1.0)
+	ax4.set_xlabel('Cache Size (KB)', fontsize=14, fontweight='bold')
+	ax4.set_ylabel('Miss Ratio', fontsize=14, fontweight='bold')
+	grid(True)
+	return [ax1, ax2, ax3, ax4]
 
 
 
@@ -125,17 +142,32 @@ def plot_ttc_curve(figure_name, title, orig_data, orig_histogram, tiled_data=Non
 		print(f"PLOT TILE TTC")
 	fig = figure(figsize=(12,8))
 	col = 1 if not tiled_data else 2
-	figure_cnt = 3*col
+	figure_cnt = 4*col
 	row = figure_cnt // col
 	figure_idx = 1
+	prev_axes = None
 	if orig_data:
-		plot_ttc_figure_entry(fig, (row, col, figure_idx), orig_data, orig_histogram, "TTC (ORIG)")
+		prev_axes = plot_ttc_figure_entry(fig, (row, col, figure_idx, prev_axes), orig_data, orig_histogram, "TTC (ORIG)")
 		figure_idx += 1
 	if tiled_data:
-		plot_ttc_figure_entry(fig, (row, col, figure_idx), tiled_data, tiled_histogram, "TTC (TILED)")
+		prev_axes = plot_ttc_figure_entry(fig, (row, col, figure_idx, prev_axes), tiled_data, tiled_histogram, "TTC (TILED)")
 		figure_idx += 1
 	fig.suptitle(title, fontsize=18)
 	savefig(f"{IMAGE}/{figure_name}")
+
+def intropolate_ttc(json_data):
+	for i in range(len(json_data)):
+		json_entry = json_data[i]
+		cache_size = json_entry[0]
+		ce = json_entry[2]
+		cs = json_entry[1]
+		real_ttc = cache_size // cs
+		print(f"Before: {json_entry[-2]} ({ce} // {cs})")
+		print(f"After: {real_ttc} ({cache_size} // {cs})")
+		json_data[i][-2] = real_ttc
+
+
+
 
 
 if __name__ == '__main__':
@@ -170,7 +202,6 @@ if __name__ == '__main__':
 			# if os.path.exists(f"{DATA}/orig/{program}-t{t}-c4-pin-rih-0.data"):
 			# 	convert_histogram_to_log2(f"{DATA}/orig/{program}-t{t}-c4-pin-rih-0.data")
 			# 	tiled_data_file = f"{DATA}/orig/{program}-t{t}-c4-pin-rih-0.data"
-
 			if orig_data_file:
 				cmd = f"TTC_LOG=INFO {TARGET}/ttc unshared --input {orig_data_file} -m {args.cache} -o {JSON}/{program}-t{t}-ttc-orig.json "
 				print(f"run {cmd} ...")
@@ -180,6 +211,13 @@ if __name__ == '__main__':
 				f = open(data_file)
 				orig_data = json.load(f)
 				orig_histogram = read_histogram(orig_data_file)
+				tiled_data = copy.deepcopy(orig_data)
+				intropolate_ttc(tiled_data)
+				tiled_histogram = read_histogram(orig_data_file)
+				f.close()
+
+
+			'''
 			if tiled_data_file:
 				cmd = f"TTC_LOG=INFO {TARGET}/ttc unshared --input {tiled_data_file} -m {args.cache} -o {JSON}/{program}-t{t}-ttc-tiled.json "
 				print(f"run {cmd} ...")
@@ -187,8 +225,10 @@ if __name__ == '__main__':
 				stdout, stderr = proc.communicate()
 				data_file = f"{JSON}/{program}-t{t}-ttc-tiled.json"
 				f = open(data_file)
-				tiled_data = json.load(f)
+				tiled_data = intropolate_ttc(json.load(f))
 				tiled_histogram = read_histogram(tiled_data_file)
+				f.close()
+			'''
 			if not orig_data and not tiled_data:
 				print(f"SKIP {program}, No histogram found")
 				continue
